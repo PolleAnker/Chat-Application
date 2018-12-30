fca054cfaa406c889bcad1a097cd5c71a2d32f4e
PolleAnker <thementoryay@gmail.com>
2018-12-30 23:21:28 +0100

    fixed minor stuff

--- client2/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ client2/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -47,5 +47,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/winsockTestClient.dir/DependInfo.cmake"
+  "CMakeFiles/client2.dir/DependInfo.cmake"
   )
--- client2/cmake-build-debug/CMakeFiles/winsockTestClient.dir/DependInfo.cmake
+++ client2/cmake-build-debug/CMakeFiles/winsockTestClient.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/theme/CLionProjects/MiniProject_PCSS/client2/main.cpp" "C:/Users/theme/CLionProjects/MiniProject_PCSS/client2/cmake-build-debug/CMakeFiles/winsockTestClient.dir/main.cpp.obj"
+  "C:/Users/theme/Desktop/chatApplication/client2/main.cpp" "C:/Users/theme/Desktop/chatApplication/client2/cmake-build-debug/CMakeFiles/winsockTestClient.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
